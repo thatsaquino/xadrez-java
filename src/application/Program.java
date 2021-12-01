c910658af5eef44290d60d178f5adff1b4377602
thatsaquino <thaisaquino1994@gmail.com>
2021-12-01 20:20:05 -0300

    Começo da implementação das peças e tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,13 @@
 package application;
 
 import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Posicao posicao = new Posicao(3, 5);
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
 	}
 
